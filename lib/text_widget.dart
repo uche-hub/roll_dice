f0de5bb76572cfb1b50c861fc317f343c646cbc5
uche-hub <ucj.theuche@gmail.com>
2025-01-07 19:20:48 +0100

    Created Global variable for text name

--- lib/text_widget.dart
+++ lib/text_widget.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 
+String textName = "Uchenna";
+
 class StyledText extends StatelessWidget {
   const StyledText({super.key});
 
   @override
   Widget build(context) {
-    return const Center(
-        child: Text(
-          "Uchenna Ndukwe",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-              color: Colors.white,
-              fontSize: 50,
-              fontFamily: 'Roboto',
-              fontWeight: FontWeight.bold),
-        ),
-      );
+    return Center(
+      child: Text(
+        textName,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            color: Colors.white,
+            fontSize: 50,
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.bold),
+      ),
+    );
   }
 }
