fdf01c936c8512fb7e24bb64e31f73fde3baa224
uche-hub <ucj.theuche@gmail.com>
2025-01-07 15:49:26 +0100

    Created a Constructor Function

--- lib/main.dart
+++ lib/main.dart
@@ -10,35 +10,33 @@ void main() {
 
 // StatelessWidget is the in-built class we are inheriting from
 // ADDS LOGIC AND DATA TO OUR CLASS
-class  GradientContainer extends StatelessWidget {
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({ super.key});
 
   // overriding a method expected by StatelessWidget
   @override
   Widget build(context) {
-    // TODO: implement build
     return Container(
-        decoration: BoxDecoration(
+      decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [ 
-              Color.fromARGB(255, 227, 171, 5),
-              Color.fromARGB(255, 106, 80, 3),
-             ],
-             begin: Alignment.topLeft,
-             end: Alignment.bottomRight,
-          )
-        ),
-        child:const Center(
-          child: Text(
-            "Uchenna Ndukwe",
-            textAlign: TextAlign.center,
-            style: TextStyle(
+        colors: [
+          Color.fromARGB(255, 227, 171, 5),
+          Color.fromARGB(255, 106, 80, 3),
+        ],
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+      )),
+      child: const Center(
+        child: Text(
+          "Uchenna Ndukwe",
+          textAlign: TextAlign.center,
+          style: TextStyle(
               color: Colors.white,
               fontSize: 50,
               fontFamily: 'Roboto',
-              fontWeight: FontWeight.bold
-            ),
-          ),
+              fontWeight: FontWeight.bold),
         ),
-      );
+      ),
+    );
   }
 }
