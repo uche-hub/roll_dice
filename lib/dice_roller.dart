c733d204baf53f62588013719ec8e38a5110b6e9
uche-hub <ucj.theuche@gmail.com>
2025-01-10 00:06:04 +0100

    Added functions to make dice button roll all 6. PROJECT COMPELETED

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
@@ -10,11 +11,12 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = "assets/images/dice-1.png";
+  final randomizer = Random();
+  var currentDiceRoll = 1;
 
   void rollDice() {
     setState(() {
-      activeDiceImage = 'assets/images/dice-4.png';
+      currentDiceRoll = randomizer.nextInt(6) + 1;
     });
   }
 
@@ -25,7 +27,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          "assets/images/dice-$currentDiceRoll.png",
           width: 200,
         ),
         TextButton(
